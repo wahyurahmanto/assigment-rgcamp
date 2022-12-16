f36e6956deb9771bc2b68cf9cf91d8b99afd444c
Wahyu Rahmanto <wahyurahmanto89@gmail.com>
2022-12-16 08:10:59 +0800

    deployment-assigment-rgcamp

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Wahyu Rahmanto" // TODO: replace this
+const studentId = "FE3123393" // TODO: replace this
+
+export { url, studentName, studentId }
